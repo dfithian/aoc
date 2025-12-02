8a5bcaec85ea1ef4e76514cab47eb442da9b2761
Dan Fithian <daniel.m.fithian@gmail.com>
2025-12-02 09:14:26 -0500

    Script to initialize a new day

---
+++ 2025/bin/new-day.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"
+today=$(date +%d)
+lastday=$(find ../src/bin -depth 1 -type f -name "*.rs" | grep -v "day-$today" | tail -n 1 | xargs basename | cut -d '.' -f 1 | cut -d '-' -f 2)
+mkdir -p "../input/day-$today"
+touch "../input/day-$today/example.txt"
+touch "../input/day-$today/input.txt"
+cp "../src/bin/day-$lastday.rs" "../src/bin/day-$today.rs"
+if ! grep "name = day-$today" ../Cargo.toml; then
+    echo ""                      >> ../Cargo.toml
+    echo "[[bin]]"               >> ../Cargo.toml
+    echo "name = \"day-$today\"" >> ../Cargo.toml
+fi
+code "../src/bin/day-$today.rs"
+code "../input/day-$today/input.txt"
+code "../input/day-$today/example.txt"
