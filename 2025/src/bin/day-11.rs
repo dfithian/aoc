6a99e3ef1e764a1d3a9d4cd5165e4d32e706b052
Dan Fithian <daniel.m.fithian@gmail.com>
2025-12-13 13:12:54 -0500

    Day 11 part 2

--- 2025/src/bin/day-11.rs
+++ 2025/src/bin/day-11.rs
@@ -1,92 +1,41 @@
-use std::collections::{BTreeMap, BTreeSet, VecDeque};
+use std::collections::{BTreeMap, BTreeSet};
 
 use aoc_2025::parse_input_file;
 
-fn walk(devices: &BTreeMap<String, BTreeSet<String>>, start: &'static str, end: &'static str, must_not_be_seen: BTreeSet<String>) -> u32 {
-    let mut routes = 0;
-    let mut possibilities = VecDeque::from([(start.to_string(), must_not_be_seen.clone())]);
-    while let Some((device, seen)) = possibilities.pop_front() {
-        if &device == end {
-            routes += 1;
-        } else if !seen.contains(&device) {
-            devices.get(&device).cloned().unwrap_or(BTreeSet::new()).into_iter().for_each(|next_device| {
-                let mut next_seen = seen.clone();
-                next_seen.insert(device.clone());
-                possibilities.push_back((next_device, next_seen));
-            })
+fn count_routes(devices: &mut BTreeMap<String, BTreeMap<String, u64>>, for_keys: BTreeSet<String>) {
+    for input in devices.clone().keys().cloned().filter(|input| !for_keys.contains(input)) {
+        let outputs = devices.get(&input).cloned().unwrap();
+        devices.remove(&input);
+        for (upstream_input, mut upstream_outputs) in devices.clone().into_iter().filter(|(_, other_outputs)| other_outputs.contains_key(&input)) {
+            let input_count = upstream_outputs.remove(&input).unwrap();
+            for (output, count) in outputs.clone().into_iter() {
+                upstream_outputs.entry(output)
+                    .and_modify(|m| *m += input_count * count)
+                    .or_insert(input_count * count);
+            }
+            devices.insert(upstream_input, upstream_outputs);
         }
     }
-    routes
 }
 
 fn main() {
-    let devices = parse_input_file(BTreeMap::<String, BTreeSet<String>>::new(), |mut acc, next| {
+    let devices = parse_input_file(BTreeMap::<String, BTreeMap<String, u64>>::new(), |mut acc, next| {
         let input = next[0..3].to_string();
-        let outputs = next[5..].to_string().split(" ").map(|s| s.to_string()).collect::<BTreeSet<String>>();
+        let outputs = next[5..].to_string().split(" ").map(|s| (s.to_string(), 1)).collect::<BTreeMap<String, u64>>();
         acc.insert(input, outputs);
         acc
     });
 
-    let part1 = walk(&devices, "you", "out", BTreeSet::new());
+    let mut part1_devices = devices.clone();
+    count_routes(&mut part1_devices, BTreeSet::from(["you".to_string(), "out".to_string()]));
+    let part1 = part1_devices.get("you").cloned().unwrap_or(BTreeMap::new()).get("out").cloned().unwrap_or(0);
     println!("part 1: {part1}");
 
-    let mut devices = devices.into_iter().map(|(input, outputs)| {
-        (
-            input,
-            outputs.into_iter().map(|output| (output, 1)).collect::<BTreeMap<String, u32>>(),
-        )
-    }).collect::<BTreeMap<String, BTreeMap<String, u32>>>();
-
-    println!("{:?}", devices);
-    let do_not_touch = BTreeSet::from(["svr".to_string(), "dac".to_string(), "fft".to_string(), "out".to_string()]);
-    let mut devices_sorted = devices.clone().into_iter().filter(|(input, _)| !do_not_touch.contains(input)).collect::<Vec<(String, BTreeMap<String, u32>)>>();
-    devices_sorted.sort_by_key(|(_, outputs)| outputs.len());
-
-    for (input, _) in devices_sorted.into_iter() {
-        let outputs = devices.get(&input).cloned().unwrap_or(BTreeMap::new());
-        let upstream_inputs = devices.clone().into_iter().filter_map(|(other_input, other_outputs)| {
-            if other_outputs.contains_key(&input) {
-                Some(other_input)
-            } else {
-                None
-            }
-        }).collect::<Vec<String>>();
-        println!("{:?}, {:?}, {:?}", input, outputs, upstream_inputs);
-        if upstream_inputs.len() > 0 {
-            devices.remove(&input);
-        }
-        for upstream_input in upstream_inputs.into_iter() {
-            devices.entry(upstream_input.clone()).and_modify(|upstream_outputs| {
-                outputs.clone().into_iter().for_each(|(output, n)| {
-                    upstream_outputs.entry(output).and_modify(|m| *m += n).or_insert(n);
-                });
-                upstream_outputs.remove(&upstream_input);
-                upstream_outputs.remove(&input);
-            });
-        }
-        println!("{:?}", devices);
-    }
-    println!("{:?}", devices);
-
-    let routes_svr = devices.get("svr").cloned().unwrap_or(BTreeMap::new());
-    let routes_dac = devices.get("dac").cloned().unwrap_or(BTreeMap::new());
-    let routes_fft = devices.get("fft").cloned().unwrap_or(BTreeMap::new());
-
-    // let routes_svr_dac = routes_svr.get("dac").cloned().unwrap_or(0);
-    // let routes_dac_fft = routes_dac.get("fft").cloned().unwrap_or(0);
-    // let routes_fft_out = routes_fft.get("out").cloned().unwrap_or(0);
-    let routes_svr_fft = routes_svr.get("fft").cloned().unwrap_or(0);
-    let routes_fft_dac = routes_fft.get("dac").cloned().unwrap_or(0);
-    let routes_dac_out = routes_dac.get("out").cloned().unwrap_or(0);
-
-    // println!("svr -> dac {:?}", routes_svr_dac);
-    // println!("dac -> fft {:?}", routes_dac_fft);
-    // println!("fft -> out {:?}", routes_fft_out);
-    println!("svr -> fft {:?}", routes_svr_fft);
-    println!("fft -> dac {:?}", routes_fft_dac);
-    println!("dac -> out {:?}", routes_dac_out);
-
-    // let part2 = (routes_svr_dac * routes_dac_fft * routes_fft_out) + (routes_svr_fft * routes_fft_dac * routes_dac_out);
+    let mut part2_devices = devices;
+    count_routes(&mut part2_devices, BTreeSet::from(["svr".to_string(), "dac".to_string(), "fft".to_string(), "out".to_string()]));
+    let routes_svr_fft = part2_devices.get("svr").cloned().unwrap_or(BTreeMap::new()).get("fft").cloned().unwrap_or(0);
+    let routes_fft_dac = part2_devices.get("fft").cloned().unwrap_or(BTreeMap::new()).get("dac").cloned().unwrap_or(0);
+    let routes_dac_out = part2_devices.get("dac").cloned().unwrap_or(BTreeMap::new()).get("out").cloned().unwrap_or(0);
     let part2 = routes_svr_fft * routes_fft_dac * routes_dac_out;
     println!("part 2: {part2}");
 }
